9eb3c498c849df5cabe782119d7bbc3573f7e73b
far120 <tatafar2018@gmail.com>
2024-10-10 03:54:10 +0300

    v2

--- src/pages/auth/auth-nav.js
+++ src/pages/auth/auth-nav.js
@@ -4,14 +4,14 @@ import "./authnav.css";
 export default function Nav() {
   const navigate = useNavigate();
   function logout() {
-    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("sign");
   
   }
   return (
     <nav className="navbar navbar-dark bg-dark nav">
       <div className="auth-nav">
         <form className="d-flex gap-3">
-          {!window.localStorage.getItem("token") ? (
+          {!window.localStorage.getItem("sign") ? (
             <>
               <Link className="navbar-brand nav-hover" to="/signup">
                 <button className="btn btn-outline-primary p-3" type="button">Signup</button>
