5824a94b5f48adcdb37602c247bdda0796de5d62
far120 <tatafar2018@gmail.com>
2024-10-11 17:52:53 +0300

    v3

--- src/pages/category/movies.js
+++ src/pages/category/movies.js
@@ -72,8 +72,8 @@ if(!movie || !movie.length===0){
           </div>
           <nav aria-label="Page navigation example" style={{display:"flex" , alignItems:"center" , justifyContent:"center"}}>
   <ul class="pagination" >
-    { id > 20 ?(
-    <li class="page-item"><button class="page-link"  onClick={()=>handelid(id-20)}>Previous</button></li>
+    { id > 1 ?(
+    <li class="page-item"><button class="page-link"  onClick={()=>handelid(id-1)}>Previous</button></li>
     ):
     (
         <li class="page-item disabled"><button class="page-link" disabled>Previous</button></li>
@@ -86,7 +86,7 @@ if(!movie || !movie.length===0){
     )}
     <li class="page-item"><button class="page-link" style={{textDecoration:"underline"}}  onClick={()=>handelid(id)}>{id}</button></li>
     <li class="page-item"><button class="page-link"  onClick={()=>handelid(id+1)}>{id+1}</button></li>
-    <li class="page-item"><button class="page-link"  onClick={()=>handelid(id+20)}>Next</button></li>
+    <li class="page-item"><button class="page-link"  onClick={()=>handelid(id+1)}>Next</button></li>
   </ul>
 </nav>
           </div>
